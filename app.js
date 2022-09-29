1b716b9f17f33a50d2be08c1f08f1b75744af658
Jam Nguyen <kietnguyen@star.global>
2022-09-29 17:24:40 +0700

    Add project.

---
+++ app.js
@@ -0,0 +1,56 @@
+import express from "express";
+import expressWs from "express-ws";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT;
+const app = express();
+expressWs(app);
+
+app.use(cors());
+app.use(express.json());
+
+const messages = [];
+const sockets = [];
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Simple Chat Room server.");
+});
+
+app.get("/messages", (req, res) => {
+  res.status(200).send(messages);
+});
+
+app.post("/messages", (req, res) => {
+  const message = req.body;
+  messages.push(message);
+
+  broadcastMessages(JSON.stringify(message));
+  res.sendStatus(200);
+});
+
+app.ws("/chat", (socket) => {
+  sockets.push(socket);
+
+  socket.on("message", (message) => {
+    messages.push(message);
+    broadcastMessages(message);
+  });
+
+  socket.on("close", () => {
+    sockets.splice(sockets.indexOf(socket), 1);
+  });
+});
+
+// UTILS
+function broadcastMessages(message) {
+  for (let i = 0; i < sockets.length; i++) {
+    sockets[i].send(message);
+  }
+}
